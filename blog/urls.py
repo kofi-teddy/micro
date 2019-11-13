431e98b1f83abc0ce8cda9e327489533e9049057
kofi teddy <agudogot@gamil.com>
2019-11-13 12:15:42 +0000

    Clean up ignored files

---
+++ blog/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+app_name = 'blog'
+urlpatterns = [
+    
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
+    path('', views.PostlistView.as_view(), name='post_list'),
+    path('<post_id>/share', views.post_share, name='post_share')
+
+    # path('year/day/month/post', views.post_detail, name='post_detail'),
+    #post views
+    # path('', views.post_list, name='post_list'),
+]
